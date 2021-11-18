023bd4b80af7a900bb2e78d12743ec94534d4ea9
PaweĹ <pawel.szapiel999@gmail.com>
2021-11-18 13:55:30 +0100

    navbar

--- cinemareact/src/Components/App.js
+++ cinemareact/src/Components/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import {Button} from "react-bootstrap";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Navbar from "./TopNavbar";
 
 const App = () => {
     return (
-        <div>
+
+        <BrowserRouter>
             <h1>Cinema</h1>
             <Button variant={"dark"} >Helloworld</Button>
-        </div>
+            <Navbar></Navbar>
+            <Switch>
+                <Route path={"/"} />
+                <Route path={"/repertuar"}/>
+                <Route path={"/cennik"} />
+                <Route path={"/filmy"} />
+                <Route path={"/ticket/:film_id/:time/:kino"}/>
+            </Switch>
+        </BrowserRouter>
+
     );
 };
 
