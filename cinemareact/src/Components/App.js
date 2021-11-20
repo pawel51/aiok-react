d7c91b70775275aaed38ecaa106632876aca6d8a
PaweĹ <pawel.szapiel999@gmail.com>
2021-11-20 13:25:28 +0100

    pobieranie obrazów do karuzeli asynchronicznie

--- cinemareact/src/Components/App.js
+++ cinemareact/src/Components/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import {Button} from "react-bootstrap";
+import {Button, Stack} from "react-bootstrap";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
-import Navbar from "./TopNavbar";
 import Films from "./Films/Films";
+import TopNavbar from "./TopNavbar";
 
 const App = () => {
     return (
 
         <BrowserRouter>
-            <h1>Cinema</h1>
-            <Button variant={"dark"} >Helloworld</Button>
-            <Navbar></Navbar>
+            <TopNavbar></TopNavbar>
             <Switch>
                 <Route exact path={"/"} />
                 <Route path={"/repertuar"}/>
                 <Route path={"/cennik"} />
-                <Route path={"/films"} render={() => <Films></Films>}/>
+                <Route path={"/films"} component={Films}>
+                </Route>
                 <Route path={"/ticket/:film_id/:time/:kino"}/>
             </Switch>
         </BrowserRouter>
--- cinemareact/src/Components/Films/Films.js
+++ cinemareact/src/Components/Films/Films.js
@@ -1,28 +1,94 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import fs from 'fs'
-import jsonReader from "../../../../../psy/src/Helpers/jsonReader";
+import axios from 'axios'
+import {Carousel} from "react-bootstrap";
+import '../../styles/films/films.css'
 
-const ids = [
-    "tt0111161",
-    "tt0068646",
-    "tt0071562",
-    "tt0468569",
-    "tt0050083"
-    ]
+
+
 
 
 
 const Films = () => {
-    const url = process.env.REACT_APP_IMDB_URL
-    const api_key = process.env.REACT_APP_IMDB_API_KEY
-    //name between //
-    const path = url+"//"+api_key
 
-    console.log(ids)
+
+    const [imgs, setImgs] = useState([]);
+
+
+
+    useEffect(() => {
+
+
+        const fetchData = async(tempImgs) => {
+            const url = process.env.REACT_APP_IMDB_URL
+            const api_key = process.env.REACT_APP_IMDB_API_KEY
+            const ids = [
+                "tt0111161",
+                "tt0068646",
+                "tt0071562",
+                "tt0468569",
+                "tt0050083"
+            ]
+            const configs = ids.map(v => {
+                return {
+                    method: 'get',
+                    url: `${url}/Title/${api_key}/${v}/images`,
+                    headers: { }
+                }
+            })
+            try {
+                const tempImgs = []
+                for (const config of configs){
+                    const response = await axios(config)
+                    tempImgs.push(response["data"]["image"])
+                }
+                setImgs(tempImgs)
+            } catch (err) {
+                console.log("error fetching img data", err)
+            }
+        }
+
+        fetchData()
+    }, [])
+
+
+
+
 
     return (
-        <div>
-
+        <div className={"films"}>
+            <Carousel variant="dark">
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={imgs[0]}
+                    />
+                    <Carousel.Caption>
+                        <h5>First slide label</h5>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={imgs[1]}
+                    />
+                    <Carousel.Caption>
+                        <h5>Second slide label</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={imgs[2]}
+                    />
+                    <Carousel.Caption>
+                        <h5>Third slide label</h5>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     );
 };
