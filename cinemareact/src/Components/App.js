86e7e8254dd019bb212672e2a1c5eb5b344e579f
PaweĹ <pawel.szapiel999@gmail.com>
2021-11-29 19:32:31 +0100

    server update

--- cinemareact/src/Components/App.js
+++ cinemareact/src/Components/App.js
@@ -5,23 +5,25 @@ import Films from "./Films/Films";
 import TopNavbar from "./TopNavbar";
 import Details from "./Films/Details";
 import Edit from "./Films/Edit";
+import Shows from "./Shows/Shows";
 
 const App = () => {
     return (
 
         <BrowserRouter>
             <TopNavbar></TopNavbar>
-            <Switch>
-                <Route exact path={"/"} />
-                {/*<Route path={"/repertuar"}/>*/}
-                {/*<Route path={"/cennik"} />*/}
-                <Route exact path={"/films"} component={Films}>
-                </Route>
-                <Route exact path={"/films/:id/details"} render={({match}) => {
-                    return <Details filmId={match.params.id}/>
-                }}>
-                </Route>
-            </Switch>
+            {/*<Switch>*/}
+            {/*    <Route exact path={"/"} />*/}
+            {/*    /!*<Route path={"/repertuar"}/>*!/*/}
+            {/*    /!*<Route path={"/cennik"} />*!/*/}
+            {/*    <Route exact path={"/films"} component={Films}>*/}
+            {/*    </Route>*/}
+            {/*    <Route exact path={"/films/:id/details"} render={({match}) => {*/}
+            {/*        return <Details filmId={match.params.id}/>*/}
+            {/*    }}>*/}
+            {/*    </Route>*/}
+            {/*</Switch>*/}
+            <Shows></Shows>
         </BrowserRouter>
 
     )
---
+++ cinemareact/src/Components/Shows/Hour.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hour = (props) => {
+    const value =
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Hour;
